b6504bc9e463781a89a062299d18f7a1edfdd107
ilhooq <contact@sphilip.com>
2019-12-18 13:40:06 +0100

    Version 1.1

--- geolocalize.php
+++ geolocalize.php
@@ -1,94 +1,91 @@
 <?php
 /**
+ * @package     Joomla.Plugin
+ * @subpackage  Jea.Geolocalize
  *
- * @package	Joomla
- * @subpackage	JEA
- * @copyright	Copyright (C) 2011 PHILIP Sylvain. All rights reserved.
- * @license		GNU/GPL, see LICENSE.txt
- * Joomla Estate Agency is free software. This version may have been modified pursuant to the
- * GNU General Public License, and as distributed it includes or is derivative
- * of works licensed under the GNU General Public License or other free or open
- * source software licenses.
- *
+ * @copyright   Copyright (C) 2007 - 2019 PHILIP Sylvain. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined( '_JEXEC' ) or die( 'Restricted access' );
-
-jimport('joomla.event.plugin');
+defined('_JEXEC') or die;
 
 /**
  * Geolocalize JEA Plugin
- *
- * @package		Joomla
- * @subpackage	JEA
- * @since 		1.5
  */
 class plgJeaGeolocalize extends JPlugin
 {
+	/**
+	 * onBeforeSaveProperty method
+	 *
+	 * @param  string         $namespace  The form namespace
+	 * @param  TableProperty  $row        The property Db row instance
+	 * @param  boolean        $is_new     True if the property is new
+	 *
+	 * @return boolean  True on success
+	 */
+	public function onBeforeSaveProperty ($namespace, $row, $is_new)
+	{
+		if (empty($row->latitude) || empty($row->longitude))
+		{
+			$db = JFactory::getDbo();
+			$language = JFactory::getLanguage();
+			$langs = explode('-', $language->getTag());
+			$lang = $langs[0];
+			$region = $langs[1];
+			$location = array();
 
-    /**
-     * onBeforeSaveProperty method
-     *
-     * Called before $row property save.
-     *
-     * @param string $namespace
-     * @param TableProperties $row
-     * @param boolean $is_new
-     * @return boolean
-     */
-    public function onBeforeSaveProperty($namespace, $row, $is_new)
-    {
-        if (empty($row->latitude) || empty($row->longitude)) {
+			if (!empty($row->address))
+			{
+				$location[] = $row->address;
+			}
 
-            $db = JFactory::getDbo();
-            $language = JFactory::getLanguage();
-            $langs  = explode('-', $language->getTag());
+			if (!empty($row->town_id))
+			{
+				$db->setQuery('SELECT `value` FROM #__jea_towns WHERE id = ' . (int) $row->town_id);
+				$town = $db->loadResult();
 
-            $lang   = $langs[0];
-            $region = $langs[1];
+				if (!empty($town))
+				{
+					$location[] = $town;
+				}
+			}
 
-            $location = array();
-            if (!empty($row->address)) {
-                $location[] = $row->address;
-            }
-            if (!empty($row->town_id)) {
-                $db->setQuery('SELECT `value` FROM #__jea_towns WHERE id = '. (int) $row->town_id);
-                $town = $db->loadResult();
-                if (!empty($town)) {
-                    $location[] = $town;
-                }
-            }
-            if (!empty($region)) {
-                $location[] = $region;
-            } else {
-                $location[] = $lang;
-            }
+			$location[] = empty($region) ? $lang : $region;
 
-            $response = $this->getGeocodeFromGoogle(implode(', ', $location));
-            if (!empty($response->results)) {
-                $coords = $response->results[0]->geometry->location;
+			$response = $this->getGeocodeFromGoogle(implode(', ', $location));
 
-                $row->latitude  = $coords->lat;
-                $row->longitude = $coords->lng;
-            } else {
-                JFactory::getApplication()->enqueueMessage('Geolocalisation not found for this location : '. implode(', ', $location), 'warning');
-            }
-        }
-        return true;
-    }
+			if (!empty($response->results))
+			{
+				$coords = $response->results[0]->geometry->location;
 
+				$row->latitude = $coords->lat;
+				$row->longitude = $coords->lng;
+			}
+			else
+			{
+				JFactory::getApplication()->enqueueMessage('Geolocalisation not found for this location : ' . implode(', ', $location), 'warning');
+			}
+		}
 
-    /**
-     * Retrieves Geocoding information from Google
-     * @param string $location address, city, state, etc.
-     * @return \stdClass
-     */
-    protected function getGeocodeFromGoogle($location) {
-        $url = 'http://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($location).'&sensor=false';
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        return json_decode(curl_exec($ch));
-    }
+		return true;
+	}
 
+	/**
+	 * Retrieves Geocoding information from Google
+	 *
+	 * @param   string  $location  address, city, state, etc.
+	 * @return  \stdClass
+	 */
+	protected function getGeocodeFromGoogle ($location)
+	{
+		$params = JComponentHelper::getParams('com_jea');
+		$key = $params->get('googlemap_api_key', '');
+
+		$url = 'https://maps.googleapis.com/maps/api/geocode/json?key=' . $key . '&address=' . urlencode($location);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		return json_decode(curl_exec($ch));
+	}
 }
